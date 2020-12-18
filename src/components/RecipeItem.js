b9ec815546122a14177f58dfb884d1d7ed7560db
Vikram Singh <ec1029231058@gmail.com>
2020-12-18 19:03:53 +0100

    update

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -3,14 +3,14 @@ import React from "react";
 const RecipeItem = props => {
     const { name, image, ingredientLines } = props;
     return (
-        <div class="card py-2 text-center">
+        <div className="card py-2 text-center">
             <img src={image} className="img-fluid w-50 mx-auto rounded-circle" alt="" />
-            <div class="card-body">
+            <div className="card-body">
                 <h5>{name}</h5>
             </div>
-            <ul class="list-group list-group-flush">
-                {ingredientLines.map(ingredient => (
-                    <li className="list-group-item">{ingredient}</li>
+            <ul className="list-group list-group-flush">
+                {ingredientLines.map((ingredient, i) => (
+                    <li className="list-group-item" key={i}>{ingredient}</li>
                 ))}
             </ul>
         </div>
